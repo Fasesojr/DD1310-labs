dce9dec4304e34fcab5d0f05c3d9ba1273a9984f
Faruk Sogutlu <faruk@n161-p150.eduroam.kth.se>
2023-09-06 13:21:22 +0200

    Nyversion_av_lab1

--- lab1.py
+++ lab1.py
@@ -3,29 +3,21 @@ def a_följd(a1, d, n):  #  funktion för aritmetisk följd
 	a_summa = n * (a1 + a2)/2
 	return a_summa
 
-def g_följd (g1, q, n):  #  funktion för aritmetisk följd
+def g_följd (g1, q, n):  #  funktion för geometrisk följd
 
 	g_summa = g1*((q**n)-1)/(q-1)
 	return g_summa
 
-print("Skriv in startvärdet (a1): ", end="")  # frågar för input för a1
-a1 = int(input())  #  sätter värdet på a1 = input och konverterar till en int
+  #  Frågar för värden som används för att skapa variablerna som behövs och sedan insättes in i funktionen a_följd
+a1 = int(input("Skriv in startvärdet (a1): "))
+d = int(input("Skriv in differensen (d): "))
+n = int(input("Skriv in antal element i följden (n): "))
+print("Den aritmetiska summan är: " + str(a_följd(a1, d, n)))
 
-print("Skriv in differensen (d): ", end="")  # frågar för input för d
-d = int(input())  #  sätter värdet på d = input och konverterar till en int
 
-print("Skriv in antal element i följden (n): ", end="")  # frågar för input för n
-n = int(input())  #  sätter värdet på n = input och konverterar till en int
-
-print("Den aritmetiska summan är: " + str(a_följd(a1, d, n)))  #  beräknar och sedan skriver ut talföljdens resultat
-
-print("Skriv in startvärdet (g1): ", end="")  # frågar för input för g1
-g1 = int(input())  #  sätter värdet på g1 = input och konverterar till en int
-
-print("Skriv in differensen (q): ", end="")  # frågar för input för q
-q = int(input())  #  sätter värdet på q = input och konverterar till en int
-
-print("Skriv in antal element i följden (n): ", end="")  # frågar för input för n
-n = int(input())  #  sätter värdet på n = input och konverterar till en int
+  #  Exakt samma sak som i förra kod bunten som fyller upp variablarna som vi behöver för geometriska följden och insätter det in i funktionen g_följd
+g1 = int(input("Skriv in startvärder (g1): "))
+q = int(input("Skriv in differensen (q): ")) 
+n = int(input("Skriv in antal element i följden (n): "))
 
 print("Den geometiska summan är: " + str(g_följd(g1, q, n)))  #  beräknar och sedan skriver ut talföljdens resultat
