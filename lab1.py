928b29b885d3f87b8ad804e35f8898b6a01e3162
Faruk Sogutlu <faruk@n161-p150.eduroam.kth.se>
2023-09-06 13:25:04 +0200

    laboration1_kod

--- lab1.py
+++ lab1.py
@@ -12,7 +12,7 @@ def g_följd (g1, q, n):  #  funktion för geometrisk följd
 a1 = int(input("Skriv in startvärdet (a1): "))
 d = int(input("Skriv in differensen (d): "))
 n = int(input("Skriv in antal element i följden (n): "))
-print("Den aritmetiska summan är: " + str(a_följd(a1, d, n)))
+print("Den aritmetiska summan är: " + str(a_följd(a1, d, n)))  #  beräknar och sedan skriver ut talföljdens summa
 
 
   #  Exakt samma sak som i förra kod bunten som fyller upp variablarna som vi behöver för geometriska följden och insätter det in i funktionen g_följd
@@ -20,4 +20,4 @@ g1 = int(input("Skriv in startvärder (g1): "))
 q = int(input("Skriv in differensen (q): ")) 
 n = int(input("Skriv in antal element i följden (n): "))
 
-print("Den geometiska summan är: " + str(g_följd(g1, q, n)))  #  beräknar och sedan skriver ut talföljdens resultat
+print("Den geometiska summan är: " + str(g_följd(g1, q, n)))  #  beräknar och sedan skriver ut talföljdens summa
