f5249fc148ff7e7135b92d834a01c3e5e4c264df
Faruk Sogutlu <faruk@n128-p27.eduroam.kth.se>
2023-09-19 16:57:58 +0200

    Lab3Fil2

--- lab3_modul.py
+++ lab3_modul.py
@@ -1,22 +1,22 @@
 
-def is_int(a):
-    try:
-        x = int(a)
-    except ValueError:
-        print("Detta är ingen heltal, försök igen!")
+def is_int(message):
+    x = "true"
+    while x != int:
+        try:
+            x = int(input(message))
+            return x
+        except ValueError:
+            print("Detta är inget heltal, försök igen!")
 
-def is_float(b):
-    try:
-        y = float(b)
-    except ValueError:
-        print("Detta är inget flyttal, försök igen!")
+def is_float(message):
+    y = "true"
+    while y != float:
+        try:
+            y = float(input(message))
+            return y
+        except ValueError:
+            print("Detta är inget flyttal, försök igen!")
 
-def a_summa(a1, d, n):  #  funktion för att beräkna en aritmetisk summa med givna parameter
-    a2 = a1 + d*(n-1)
-    aritmetisk_summa = n*(a1 + a2)/2
-    return aritmetisk_summa
+    
 
-def g_summa(g1, q, n):  #  funktion för att beräkna en geometrisk summa med givna parameter
-   geometrisk_summa = g1*((q**n)-1)/(q-1)
-   return geometrisk_summa
 
